8e9357a7ed22d051075a954a933114ec0015f7b8
piledriven1 <sto08@calpoly.edu>
2025-10-05 23:32:33 -0700

    Changed the header file; forgot to commit earlier

--- extFrame.hpp
+++ extFrame.hpp
@@ -1,8 +1,8 @@
 #ifndef EXTFRAME_HPP
 #define EXTFRAME_HPP
 
-cv::Mat grayscale(const cv::Mat &frame);
-void sobelFilter(const cv::Mat &frame);
-void saveImage(std::string name, const cv::Mat &frame);
+cv::Mat grayscale(const cv::Mat &frame, std::string name);
+void sobelFilter(const cv::Mat &frame, std::string name);
+void saveImage(const cv::Mat &frame, std::string name);
 
 #endif
