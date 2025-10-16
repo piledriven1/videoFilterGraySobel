1fa6a12d94ca611877eb4b219b5971ca06b479a5
Yexall <Yexall@yahoo.com>
2025-10-15 23:59:20 -0700

    Added previous documents

---
+++ README.md
@@ -0,0 +1,5 @@
+# cpe442-tutorial2
+
+## OpenCV Tutorial
+
+This program is meant to be a tutorial to using OpenCV and add grayscale or Sobel filtering to a video file.
---
+++ extFrame.hpp
@@ -0,0 +1,17 @@
+#ifndef EXTFRAME_HPP
+#define EXTFRAME_HPP
+
+#include <iostream>
+#include <string>
+
+#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/videoio.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+
+cv::Mat grayscale(const cv::Mat &frame);
+cv::Mat sobelFilter(const cv::Mat &frame);
+void saveImage(const cv::Mat &frame, std::string name);
+
+#endif
